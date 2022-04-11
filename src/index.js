0c7dc8d8cd1cbf87d02bdc0fd336caf8f4047006
Dameon Laird <dameonlair@rocketmail.com>
2022-04-11 16:55:18 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,56 +1,116 @@
 import ProjectList from "./ProjectList.js";
 customElements.define("project-list", ProjectList);
 
+function createWindow(labelText, htmlContent) {
+    let newWindow = document.querySelector("#atariStWindowTemplate").content.firstElementChild.cloneNode(true);
+    newWindow.querySelector("atariStWindowTitlebarLabel").innerText = labelText;
+
+}
+
+function setUpWindowDrag(titlebar, newWindow) {
+    let dragging = false;
+    let startOffsetX = 0;
+    let startOffsetY = 0;
+
+    let dragStart = (event) => {
+        let x = (event.clientX) ? event.clientX : event.touches[0].clientX;
+        let y = (event.clientY) ? event.clientY : event.touches[0].clientY;
+        startOffsetX = x - newWindow.offsetLeft;
+        startOffsetY = y - newWindow.offsetTop;
+        dragging = true;
+    }
+
+    let dragMove = (event) => {
+        if (dragging) {
+            let x = (event.clientX) ? event.clientX : event.touches[0].clientX;
+            let y = (event.clientY) ? event.clientY : event.touches[0].clientY;
+            newWindow.style.left = x - startOffsetX + "px";
+            newWindow.style.top = y - startOffsetY + "px";
+        }
+    }
+
+    let dragEnd = (event) => {
+        dragging = false;
+    }
+
+    titlebar.addEventListener("mousedown", dragStart);
+    window.addEventListener("mousemove", dragMove);
+    window.addEventListener("mouseup", dragEnd);
+
+    titlebar.addEventListener("touchstart", dragStart);
+    window.addEventListener("touchmove", dragMove);
+    window.addEventListener("touchend", dragEnd);
+}
+
+function setUpCloseButton(closeButton, newWindow) {
+
+}
+
 document.querySelectorAll(".atariStDesktopItem").forEach(element => {
     element.addEventListener("click", () => {
-        let newWindow = element.querySelector(".atariStDesktopItemWindowTemplate").content.firstElementChild.cloneNode(true);
+        let newWindow = element.querySelector(".atariStWindowTemplate").content.firstElementChild.cloneNode(true);
         newWindow.style.display = "block";
         
         document.querySelector("#atariStDesktopItems").appendChild(newWindow);
 
         let innerWindow = newWindow.querySelector("[loadFromUrl]");
         if (innerWindow) {
-            newWindow.querySelector(".atariStDesktopItemWindowContent").style.overflow = "hidden";
+            newWindow.querySelector(".atariStWindowContent").style.overflow = "hidden";
             innerWindow.setAttribute("src", innerWindow.getAttribute("loadFromUrl"));
         }
 
-        newWindow.querySelector(".atariStDesktopItemWindowTitlebarClose").addEventListener("click", () => {
+        newWindow.querySelector(".atariStWindowTitlebarClose").addEventListener("click", () => {
             newWindow.addEventListener("animationend", () => newWindow.remove());
             newWindow.setAttribute("windowCloseAnimation", "");
         });
 
-        let dragging = false;
-        let titleBar = newWindow.querySelector(".atariStDesktopItemWindowTitlebar");
-        let startOffsetX = 0;
-        let startOffsetY = 0;
+        let titlebar = newWindow.querySelector(".atariStWindowTitlebar");
+        setUpWindowDrag(titlebar, newWindow);
+    });
+});
 
-        let dragStart = (event) => {
-            let x = (event.clientX) ? event.clientX : event.touches[0].clientX;
-            let y = (event.clientY) ? event.clientY : event.touches[0].clientY;
-            startOffsetX = x - newWindow.offsetLeft;
-            startOffsetY = y - newWindow.offsetTop;
-            dragging = true;
+document.body.addEventListener("click", (event) => {
+    let target = event.target;
+    while (target != document.body) {
+        if (target.id === "atariStMenuBar") {
+            break;
         }
+        target = target.parentElement;
+    }
 
-        let dragMove = (event) => {
-            if (dragging) {
-                let x = (event.clientX) ? event.clientX : event.touches[0].clientX;
-                let y = (event.clientY) ? event.clientY : event.touches[0].clientY;
-                newWindow.style.left = x - startOffsetX + "px";
-                newWindow.style.top = y - startOffsetY + "px";
-            }
+    if (target == document.body) {
+        document.querySelector("#atariStMenuBarResponsive").style.display = "";
+
+        document.querySelectorAll(".atariStMenuFoldout").forEach(element => {
+            element.style.display = "";
+        });
+    }
+});
+
+document.querySelector("#atariStMenuBarResponsiveLabel").addEventListener("click", () => {
+    document.querySelector("#atariStMenuBarResponsive").style.display = "block";
+});
+
+document.querySelectorAll(".atariStMenuFoldout").forEach(element => {
+    element.previousElementSibling.addEventListener("click", () => {
+        element.style.display = (element.style.display == "") ? "flex" : "";
+    });
+
+    document.addEventListener("click", (event) => {
+        if ((event.target != element.previousElementSibling) && (!element.contains(event.target))) {
+            element.style.display = "";
         }
+    });
+});
 
-        let dragEnd = (event) => {
-            dragging = false;
+document.querySelectorAll(".atariStMenuItem, .atariStMenuFoldoutItem").forEach(element => {
+    element.addEventListener("click", () => {
+        element.classList.add("atariStMenuItemActive");
+    });
+
+    document.addEventListener("click", (event) => {
+        if (event.target != element && !element.contains(event.target)) {
+            element.classList.remove("atariStMenuItemActive");
         }
-
-        titleBar.addEventListener("mousedown", dragStart);
-        window.addEventListener("mousemove", dragMove);
-        window.addEventListener("mouseup", dragEnd);
-
-        titleBar.addEventListener("touchstart", dragStart);
-        window.addEventListener("touchmove", dragMove);
-        window.addEventListener("touchend", dragEnd);      
     });
 });
